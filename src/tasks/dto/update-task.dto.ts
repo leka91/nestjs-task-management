c10abb580e14544247d961f6a5991c8c60afc6a9
Aleksandar Krasic <alex.k@referrizer.com>
2025-03-29 18:15:57 +0100

    validation pipes

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,3 +1,7 @@
+import { IsEnum } from 'class-validator';
+import { TaskStatus } from '../enum/task-status.enum';
+
 export class UpdateTaskDto {
+  @IsEnum(TaskStatus)
   status: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task } from './task.model';
 import { TaskStatus } from './enum/task-status.enum';
 import { v7 as uuid } from 'uuid';
@@ -14,11 +14,18 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return task;
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const foundTask: Task = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== foundTask.id);
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -37,7 +44,7 @@ export class TasksService {
   }
 
   updateTask(id: string, updateTaskDto: UpdateTaskDto): Task {
-    const task: Task = this.tasks.find((task) => task.id === id);
+    const task: Task = this.getTaskById(id);
     const { status } = updateTaskDto;
 
     task.status = TaskStatus[status];
